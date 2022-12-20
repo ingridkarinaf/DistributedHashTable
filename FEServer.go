584660d31838a27ed4f8f6974370001343247d25
ingrid forre <ingridkforre@gmail.com>
2022-12-20 12:13:58 +0100

    handles max one FE and one RM crashing

--- FEServer.go
+++ FEServer.go
@@ -24,7 +24,6 @@ Dials to pre-defined replica manager servers, i.e. 5000, 5001 and 5002
 type FEServer struct {
 	hashtable.UnimplementedHashTableServer        // You need this line if you have are(?) a server
 	port                    string // Not required but useful if your server needs to know what port it's listening to
-	primaryServer           hashtable.HashTableClient
 	ctx                     context.Context
 	replicaManagers 		map[string]hashtable.HashTableClient
 }
@@ -49,7 +48,6 @@ func main() {
 	defer cancel()
 	server := &FEServer{
 		port:          os.Args[1],
-		primaryServer: nil,
 		ctx:           ctx,
 		replicaManagers: make(map[string]hashtable.HashTableClient),
 	}
@@ -81,13 +79,10 @@ func main() {
 }
 
 func (FE *FEServer) DialToServer(port string) (*grpc.ClientConn) {
-	fmt.Println("port inside dial to server: ", port)
-	//Add connection to map
-	// var conn *grpc.ClientConn
-	log.Printf("RMServer %v: Trying to dial: %v\n", port)
+	log.Printf("FE server %v: Trying to dial RM server with port: %v\n", FE.port, port)
 	conn, err := grpc.Dial(port, grpc.WithInsecure(), grpc.WithBlock()) //This is going to wait until it receives the connection
-	if err != nil { // //Reconsider error handling
-		log.Fatalf("FEServer could not connect: %s", err)
+	if err != nil { //Reconsider error handling
+		log.Fatalf("FEServer %s could not connect to RM server with port: %s", FE.port, port, err)
 	}
 	
 	c := hashtable.NewHashTableClient(conn)
@@ -98,25 +93,22 @@ func (FE *FEServer) DialToServer(port string) (*grpc.ClientConn) {
 //Waits only for two success responses, chucks out the last one (for performance, only a bonus if the last one is successful)
 func (FE *FEServer) Put(ctx context.Context, hashUpt *hashtable.PutRequest) (*hashtable.PutResponse, error){
 
-	//var wg sync.WaitGroup
 	resultChannel := make(chan bool, 2)
-	for _, RMconnection := range FE.replicaManagers  {
-		//wg.Add(1)
-		fmt.Println("rm connection: ", RMconnection)
+	for port, RMconnection := range FE.replicaManagers  {
 		
-		go func(connection hashtable.HashTableClient) {
-			//Reconsider if something should be done with return value 
+		go func(rmPort string, connection hashtable.HashTableClient) {
 			_, err := connection.Put(context.Background(), hashUpt) //does context.background make it async?
 			if err != nil {
-				fmt.Printf("Map update failed: %s", err) //identify which replica server?
+				fmt.Printf("Hash table to RM server update failed for FE server %s: %s", rmPort, FE.port, err) //identify which replica server?
 			} else {
 				resultChannel <- true
 			}
-		}(RMconnection)
+		}(port, RMconnection)
 	}
-	//wg.Wait()
-	resp1, resp2 := <-resultChannel, <-resultChannel // Should wait until received two values
-	fmt.Println("Responses: ", resp1, resp2)
+	
+	// Should wait until received two values
+	<-resultChannel
+	<-resultChannel 
 
 	hashtableUpdateOutcome := &hashtable.PutResponse{
 		Success: true,
@@ -125,27 +117,24 @@ func (FE *FEServer) Put(ctx context.Context, hashUpt *hashtable.PutRequest) (*ha
 }
 
 func (FE *FEServer) Get(ctx context.Context, getRsqt *hashtable.GetRequest) (*hashtable.GetResponse, error) {
-
 	responseChannel := make(chan *hashtable.GetResponse, 2)
-	for _, RMconnection := range FE.replicaManagers  {
-		fmt.Println("RM connection: ", RMconnection)
-		go func(connection hashtable.HashTableClient) {
-			fmt.Println("RM connection inside thread: ", connection)
+	for port, RMconnection := range FE.replicaManagers  {
+		go func(rmPort string, connection hashtable.HashTableClient) {
 			result, err := connection.Get(context.Background(), getRsqt) 
 			if err != nil {
-				fmt.Printf("Map update failed: %s", err)
+				fmt.Printf("Hash table update to RM server %s failed in FE server %s: %s", rmPort, FE.port, err)
+				delete(FE.replicaManagers, rmPort)
 			} else {
-				fmt.Println("result inside get before passing to channel: ", result )
 				responseChannel <- result
 			}
-		}(RMconnection)
+		}(port, RMconnection)
 	}
 
-	resp1, resp2 := <-responseChannel, <-responseChannel
-	fmt.Println("responses inside get of FE server: ", resp1, resp2)
-	if resp1 != resp2 {
+	resp1 := <-responseChannel
+	resp2 :=  <-responseChannel
+
+	if resp1.Value != resp2.Value {
 		resp3 := <-responseChannel
-		fmt.Println("responses inside get of FE server: if statement where resp1 and 2 arent equal ", resp1, resp2, resp3)
 		return resp3, nil
 	}
 	return resp1, nil
--- client.go
+++ client.go
@@ -21,9 +21,11 @@ Responsible for:
 Limitations:
 	1. Can only dial to pre-determined front-ends or by incrementing 
 	(then there is no guarantee that there is an FE with that port number)
+	2. Assumes a failed request is due to a crashed server, redials immediately
 */
 
-var i32 interface{} = int32(5)
+var server hashtable.HashTableClient
+var connection *grpc.ClientConn 
 
 func main() {
 
@@ -31,14 +33,14 @@ func main() {
 	f := setLogClient()
 	defer f.Close()
 
-	//clientPort := ":" + os.Args[2]
-	FEport := ":" + os.Args[2] //dial 4000 or 4001 (available ports on the FEServers)
-	connection, err := grpc.Dial(FEport, grpc.WithInsecure())
+	FEport := ":" + os.Args[1] //dial 4000 or 4001 (available ports on the FEServers)
+	conn, err := grpc.Dial(FEport, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Unable to connect: %v", err)
 	}
+	connection = conn
 
-	server := hashtable.NewHashTableClient(connection) //creates a connection with an FE server
+	server = hashtable.NewHashTableClient(connection) //creates a connection with an FE server
 	defer connection.Close()
 
 	go func() {
@@ -50,81 +52,98 @@ func main() {
 			textChoice := scanner.Text()
 			if (textChoice == "update") {
 				println("Enter key and value, separated by a space (integers only!)")
+				scanner.Scan()
 				text := scanner.Text()
 				inputArray := strings.Fields(text)
 				
 				key, err := strconv.Atoi(inputArray[0])
 				if err != nil {
-					log.Fatalf("Couldn't convert key to int: ", err)
+					fmt.Println("Couldn't convert key to int: ", err)
+					//log.Fatalf("Couldn't convert key to int: ", err)
 				}
 				value, err := strconv.Atoi(inputArray[1])
 				if err != nil {
-					log.Fatalf("Couldn't convert value to int: ", err)
+					fmt.Println("Couldn't convert key to int: ", err)
+					//log.Fatalf("Couldn't convert value to int: ", err)
 				}
 	
-				//Send request to update hashtable
 				hashtableUpdate := &hashtable.PutRequest{
 					Key:   int32(key),
 					Value: int32(value),
 				}
-	
-				fmt.Println(hashtableUpdate)
-	
-				result := Put(hashtableUpdate, connection, server)
-				fmt.Println("Put result: ", result)
+
+				result := Put(hashtableUpdate)
+				if result.Success == true {
+					fmt.Printf("Hashtable successfully updated to %v for key %v.\n", key, value)
+				} else {
+					fmt.Println("Update unsuccessful, please try again.")
+				}
+				
 			} else if (textChoice == "get") {
 				println("Enter the key of the value you would like to retireve (integers only!): ")
 				scanner.Scan()
 				text := scanner.Text()
-				val, err := strconv.Atoi(text)
+				key, err := strconv.Atoi(text)
 				if err != nil {
 					fmt.Println("Could not convert key to integer: ", err)
+					//log.Fatalf("Could not convert key to integer: ", err)
 				}
 				getReq := &hashtable.GetRequest{
-					Key:   int32(val),
+					Key:   int32(key),
 				}
 
-				result := Get(getReq, connection, server)
-				fmt.Println("Get result: ", result)
+				result := Get(getReq) //result := Get(getReq, connection, server)
+				fmt.Printf("Value of key %s: %v \n",text, int(result))
 			} else {
 				fmt.Println("Sorry, didn't catch that. ")
 			}
-			
 		}
 	}()
 
-	for {
-
-	}
+	for {}
 }
 
 //If function returns an error, redial to other front-end and try again
-func Put(hashUpt *hashtable.PutRequest, connection *grpc.ClientConn, server hashtable.HashTableClient) (*hashtable.PutResponse) {
+func Put(hashUpt *hashtable.PutRequest) (*hashtable.PutResponse) {
 	result, err := server.Put(context.Background(), hashUpt) //What does the context.background do?
 	if err != nil {
-		fmt.Printf("Client %s hashUpdate failed:%s", connection.Target(), err)
+		fmt.Printf("Client %s hashUpdate failed:%s. \n Redialing and retrying. \n", connection.Target(), err)
+		Redial()
+		return Put(hashUpt)
 	}
 	return result
 }
 
-func Get(getRsqt *hashtable.GetRequest, connection *grpc.ClientConn, server hashtable.HashTableClient) (int32) {
+func Get(getRsqt *hashtable.GetRequest) (int32) {
 	result, err := server.Get(context.Background(), getRsqt)
 	if err != nil {
 		fmt.Printf("Client %s get request failed: %s", connection.Target(), err)
 		Redial()
+		return Get(getRsqt)
 	}
 
-	if reflect.ValueOf(result.Value).Kind() !=  reflect.ValueOf(int32(5)).Kind() {
-		fmt.Println(reflect.ValueOf(int32(5)).Kind())
+	if reflect.ValueOf(result.Value).Kind() != reflect.ValueOf(int32(5)).Kind() {
 		return 0
 	}
-	fmt.Println("result inside get of client: ", result, reflect.TypeOf(result))
 	return result.Value
 }
 
 //In the case of losing connection - alternates between predefined front-
 func Redial() {
+	var port string
+	if connection.Target()[len(connection.Target())-1:] == "1" {
+		port =  ":4000"
+	} else {
+		port = ":4001"
+	}
 
+	conn, err := grpc.Dial(port, grpc.WithInsecure())
+	if err != nil {
+		fmt.Println("Unable to connect to port %s: %v", port, err)
+	}
+
+	connection = conn
+	server = hashtable.NewHashTableClient(connection) //creates a connection with an FE server
 }
 
 
